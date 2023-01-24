912d490a19d92f54d5d42558298d0807f502ca2f
GZod01 <95311062+GZod01@users.noreply.github.com>
2023-01-24 20:16:35 +0100

    ok

--- play.bat
+++ play.bat
@@ -1,10 +1,21 @@
 echo checking internet connection
 Ping www.google.fr -n 1 -w 1000
 cls
-if errorlevel 1 (set internet=Not connected to internet) else (set internet=Connected to internet)
+if errorlevel 1 (set internet="Not connected to internet") else (set internet="Connected to internet")
 
 echo %internet%
-if %internet% == Connected to internet {
-    curl https://google.com > .\google.html
-}
-start "" http://www.stackoverflow.com
+if %internet% == "Connected to internet" (
+    curl https://raw.githubusercontent.com/GZod01/USCPM/main/index.html > ".\uscpm.html"
+    echo "do"
+    start "" ".\uscpm.html"
+)
+else (
+    if exist ".\uscpm.html" (
+        start "" ".\uscpm.html"
+    ) else (
+        msg "%USERNAME%" "Sorry, your computer can't access to internet and the USCPM file doesn't exist, if the error persist create an issue in the github: https://github.com/gzod01/uscpm"
+        # echo "x=msgbox("Sorry, your computer can't access to internet and the USCPM file doesn't exist, if the error persist create an issue in the github: https://github.com/gzod01/uscpm", Button+Icon, "ERROR USCPM")" > .\message.vbs
+        # .\message.vbs
+        # del .\message.vbs
+    )
+)
