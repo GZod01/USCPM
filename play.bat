2f73e1db524e66ce6d1422e09875597badba2d24
GZod01 <95311062+GZod01@users.noreply.github.com>
2023-01-24 19:55:56 +0100

    ok

--- play.bat
+++ play.bat
@@ -1,1 +1,10 @@
+echo checking internet connection
+Ping www.google.fr -n 1 -w 1000
+cls
+if errorlevel 1 (set internet=Not connected to internet) else (set internet=Connected to internet)
 
+echo %internet%
+if %internet% == Connected to internet {
+    curl https://google.com > .\google.html
+}
+start "" http://www.stackoverflow.com
